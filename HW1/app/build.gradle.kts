de8e881e85efe507fa02377d39a406401ca3e915
hannamalinen <125193379+hannamalinen@users.noreply.github.com>
2024-02-02 13:05:17 +0200

    Update build.gradle.kts for HW3

--- HW1/app/build.gradle.kts
+++ HW1/app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    // id("com.google.devtools.ksp")
 }
 
 android {
@@ -51,9 +54,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -68,8 +71,18 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     val nav_version = "2.7.6"
-    val material = "1.5.4"
+    val material = "1.6.0"
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.compose.material:material-icons-core:$material")
     implementation("androidx.compose.material:material-icons-extended:$material")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
 }
