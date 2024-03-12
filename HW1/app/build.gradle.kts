495eae4358ee2d98204e485ec542286725dc369e
hannamalinen <125193379+hannamalinen@users.noreply.github.com>
2024-03-12 13:22:33 +0200

    Update build.gradle.kts for project v1

--- HW1/app/build.gradle.kts
+++ HW1/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.hw1_composetutorial"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -70,8 +71,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    val nav_version = "2.7.6"
-    val material = "1.6.0"
+    val nav_version = "2.7.7"
+    val material = "1.6.3"
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.compose.material:material-icons-core:$material")
     implementation("androidx.compose.material:material-icons-extended:$material")
@@ -84,5 +85,18 @@ dependencies {
     kapt("androidx.room:room-compiler:$room_version")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.3")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    val camerax_version = "1.3.2"
+    implementation("androidx.camera:camera-core:1.3.2")
+    implementation("androidx.camera:camera-camera2:$camerax_version")
+    implementation("androidx.camera:camera-lifecycle:$camerax_version")
+    implementation("androidx.camera:camera-view:1.3.2")
+
+    implementation("androidx.activity:activity-compose:1.8.2")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
